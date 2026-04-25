59aa8ca6821e6377aad0b79ffaced1f10598c3e4
AmaliaVaro <amivaro1605@gmail.com>
2026-04-25 23:24:09 +0200

    p5

---
+++ Backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CARRITO (id_usuario, correo_usuario, precio_total)
+VALUES (1, 'amalia@test.com', 0.0);
+INSERT INTO PRODUCTO (id, nombre, precio) VALUES (1, 'Brillo de labios', 10.0);
+INSERT INTO PRODUCTO (id, nombre, precio) VALUES (2, 'Colorete en polvo', 49.9);
